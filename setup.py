8b3ad60cac2b395b6b2e7cfb142d7e6ad482ec33
Andy McKay <amckay@mozilla.com>
2012-10-11 09:29:02 -0700

    add in url and email

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup
 setup(
     name='schematic',
     version='0.2',
+    url='https://github.com/mozilla/schematic',
+    author_email='dev-webdev@lists.mozilla.org',
     scripts=['schematic'],
 )
